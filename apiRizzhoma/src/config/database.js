6ac282ffa304b7f115798fdcc55f0af4cec06882
JohnnyJohnson11 <seanhimawan@gmail.com>
2025-05-23 22:54:07 +0700

    Mengganti database.js

--- apiRizzhoma/src/config/database.js
+++ apiRizzhoma/src/config/database.js
@@ -3,11 +3,10 @@ require('dotenv').config();
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
-  // 1.1 Lengkapi koneksi database dengan environment variables yang sesuai (dari env). Contoh ada di line 5.
-  user: process.env.DB_USER, 
-  password: process.env.DB_PASSWORD,   
-  database: process.env.DB_NAME, 
-  port: process.env.DB_PORT || 3306, 
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 module.exports = connection;
