7f5a31dcc431aa2dec5588ec441b1ccbcb925412
JohnnyJohnson11 <seanhimawan@gmail.com>
2025-06-04 08:53:03 +0700

    memperbaiki api uploadImage

--- apiRizzhoma/src/routes/uploadRoutes.js
+++ apiRizzhoma/src/routes/uploadRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 // Setup storage Multer
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    cb(null, 'apiRizzhoma/uploads/');
   },
   filename: (req, file, cb) => {
     const uniqueName = Date.now() + '-' + Math.round(Math.random() * 1E9) + path.extname(file.originalname);
